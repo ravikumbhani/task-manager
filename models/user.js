5530f73a1cb290676eeb436b9cdb02f2fd2d2a6a
Ravi Kumbhani <ravikumbhani007@gmail.com>
2025-01-16 21:28:20 +0530

    user model created

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a name'],
+      trim: true,
+      maxlength: [50, 'Name cannot exceed 50 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide an email'],
+      unique: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        'Please provide a valid email',
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: [6, 'Password must be at least 6 characters long'],
+      select: false,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Pre-save middleware to hash passwords
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+
+// Method to check if password matches
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+// Method to generate a JWT token
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id, role: this.role }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
